3b36aa43cd14711bf342de79e6d834fa4da65189
Andrew-Bageant <bageant@gmail.com>
2026-07-17 15:41:39 -0700

    updates Python properties to exclude 3.14.1, to avoid torchvision issues. Also added a universe source to the Monster data structure

--- src/schemas/monster.py
+++ src/schemas/monster.py
@@ -12,6 +12,7 @@ from pydantic import BaseModel, Field
 
 class MonsterMetadata(BaseModel):
     monster_id: str = Field(..., description="Unique alphanumeric identifier.")
+    monster_source: str = Field(..., description="Creative universe where the monster originated.")
     name: str = Field(..., description="Name of the creature asset.")
     description: str = Field(
         ...,
